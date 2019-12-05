024413c6a5c4090400c9dabd282d15c66e7f78df
Shellrean <reanyusufali@gmail.com>
2019-12-05 14:44:11 +0700

    Sync table params

--- app/Http/Controllers/API/v2/PusatController.php
+++ app/Http/Controllers/API/v2/PusatController.php
@@ -17,41 +17,107 @@ use App\File;
 
 class PusatController extends Controller
 {
+    // public function sinkron(Request $request)
+    // {
+    // 	$matpels = Matpel::all();
+
+    // 	$banksoal = Banksoal::where([
+    //         'server_name'   => $request->server_name
+    //     ])->get();
+    	
+    //     $soal = Soal::all();
+    // 	$jawaban = JawabanSoal::all();
+    //     $jadwal = Jadwal::all();
+    //     $directory = Directory::all();
+    //     $files = File::all();
+
+    //     $peserta = Peserta::where([
+    //         'name_server'   => $request->server_name
+    //     ])->get();
+
+    // 	$data = [
+    // 		'status'		=> 'oke',
+    // 		'matpels'		=> $matpels,
+    // 		'banksoal'		=> $banksoal,
+    // 		'soal'			=> $soal,
+    // 		'jawaban'		=> $jawaban,
+    //         'jadwal'        => $jadwal,
+    //         'peserta'       => $peserta,
+    //         'directory'     => $directory,
+    //         'files'         => $files
+    // 	];
+
+    //     $server = Server::where(['server_name' => $request->server_name])->first();
+    //     $server->sinkron = 1;
+    //     $server->save();
+
+    // 	return response()->json(['data' => $data]);
+    // }
     public function sinkron(Request $request)
     {
-    	$matpels = Matpel::all();
+        $matpels = Matpel::all();
 
-    	$banksoal = Banksoal::where([
+        $banksoal = Banksoal::where([
             'server_name'   => $request->server_name
         ])->get();
-    	
-        $soal = Soal::all();
-    	$jawaban = JawabanSoal::all();
-        $jadwal = Jadwal::all();
-        $directory = Directory::all();
-        $files = File::all();
 
-        $peserta = Peserta::where([
-            'name_server'   => $request->server_name
-        ])->get();
-
-    	$data = [
-    		'status'		=> 'oke',
-    		'matpels'		=> $matpels,
-    		'banksoal'		=> $banksoal,
-    		'soal'			=> $soal,
-    		'jawaban'		=> $jawaban,
-            'jadwal'        => $jadwal,
-            'peserta'       => $peserta,
-            'directory'     => $directory,
-            'files'         => $files
-    	];
-
-        $server = Server::where(['server_name' => $request->server_name])->first();
-        $server->sinkron = 1;
-        $server->save();
-
-    	return response()->json(['data' => $data]);
+        switch ($request->req) {
+            case 'peserta':
+                $peserta = Peserta::where([
+                    'name_server'   => $request->server_name
+                ])->get();
+                $data = [
+                    'table' => 'pesertas',
+                    'data'  => $peserta
+                ];
+                break;
+            case 'matpel':
+                $matpels = Matpel::all();
+                $data = [
+                    'table'  => 'matpels',
+                    'data'   => $matpels   
+                ];
+                break;
+            case 'banksoal':
+                $banksoal = Banksoal::where([
+                    'server_name'   => $request->server_name
+                ])->get();
+                $data = [
+                    'table'  => 'banksoals',
+                    'data'   => $banksoal   
+                ];
+                break;
+            case 'soal':
+                $soal = Soal::all();
+                $data = [
+                    'table'  => 'soals',
+                    'data'   => $soal   
+                ];
+                break;
+            case 'jawaban_soal':
+                $jawaban_soal = JawabanSoal::all();
+                $data = [
+                    'table'  => 'jawaban_soals',
+                    'data'   => $jawaban_soal   
+                ];
+                break;
+            case 'jadwal':
+                $jadwal = Jadwal::all();
+                $data = [
+                    'table'  => 'jadwals',
+                    'data'   => $jadwal   
+                ];
+                break;
+            case 'file': 
+                $files = File::all();
+                $data = [
+                    'files'  => $files,
+                ];
+                break;
+            default:
+                echo 'oke';
+        }
+        return response()->json(['data' => $data]);
     }
 
     public function connect(Request $request) 
